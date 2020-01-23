9903d9b6cb5c7e71629841927c0d9cb4113d220a
Raphael Kogler <kogler@pik-potsdam.de>
2020-01-23 17:26:22 +0100

    post-processing added

--- Testcase1.jl
+++ Testcase1.jl
@@ -2,6 +2,9 @@ using LinearAlgebra
 using CSV
 using Plots
 
+VBase = 400
+PBase = 1e4
+IBase = PBase/VBase
 
 # define structs for measurement data and complex phasor representation
 ################################################################################
@@ -12,42 +15,54 @@ mutable struct measurement
 end
 
 mutable struct phasor
-    t; u; uRot; uAmp; uPhi; i; iRot; iAmp; iPhi; P; Q; f
+    t; u; uRot; uAmp; uPhi; i; iRot; iAmp; iPhi; P; Q; f; fAvg
 end
 ################################################################################
 
-# define transformations to complex phasor representation
+# define transformations to complex phasor representation with post-processing
 ################################################################################
 ABTransform = sqrt(2.0/3.0)*[1.0 -0.5 -0.5;
                              0.0 sqrt(3.0)*0.5 -sqrt(3.0)*0.5;
                              1.0/sqrt(2.0) 1.0/sqrt(2.0) 1.0/sqrt(2.0)]
 
+function averageWin(data, winSize::Int)
+    output = copy(data)
+    for k in 1:length(data)-winSize
+        output[k] = sum(data[k:k+winSize-1])/winSize
+    end
+    return output
+end
+
 function ToComplex(meas::measurement)
-    u_ab = ABTransform*transpose(hcat(meas.U1, meas.U2, meas.U3))
-    u = u_ab[1,:] .+ im*u_ab[2,:]
     f = meas.f
-    uRot = u.*exp.(im*2*pi*50.0*meas.t)
-    uAmp = abs.(uRot)
-    uPhi = angle.(uRot)
+    fAvg = sum(f[1000:end])/length(f[1000:end])
+    win = trunc(Int, 1/(meas.t[2]*fAvg))
+    u_ab = ABTransform*transpose(hcat(meas.U1, meas.U2, meas.U3))
+    u = (u_ab[1,:] .+ im*u_ab[2,:])/VBase
+    uRot = u.*exp.(im*2*pi*fAvg*meas.t)
+    uRotAvg = averageWin(uRot, win)
+    uAmp = abs.(uRotAvg)
+    uPhi = angle.(uRotAvg)
     i_ab = ABTransform*transpose(hcat(meas.I1, meas.I2, meas.I3))
-    i = i_ab[1,:] .+ im*i_ab[2,:]
-    iRot = i.*exp.(im*2*pi*50.0*meas.t)
-    iAmp = abs.(iRot)
-    iPhi = angle.(iRot)
-    S = uRot.*conj.(iRot)
+    i = (i_ab[1,:] .+ im*i_ab[2,:])/IBase
+    iRot = i.*exp.(im*2*pi*fAvg*meas.t)
+    iRotAvg = averageWin(iRot, win)
+    iAmp = abs.(iRotAvg)
+    iPhi = angle.(iRotAvg)
+    S = uRotAvg.*conj.(iRotAvg)
     P = real.(S)
     Q = imag.(S)
-    return phasor(meas.t, u, uRot, uAmp, uPhi, i, iRot, iAmp, iPhi, P, Q, f)
+    return phasor(meas.t, u, uRot, uAmp, uPhi, i, iRot, iAmp, iPhi, P, Q, f, fAvg)
 end
 ################################################################################
 
 # read csv files for slack and load and transform to phasor representation
 ################################################################################
-path = "/home/kogler/Julia/tecnalia/Tests_Day_1/TC1.3_Box1.csv"
+path = "/home/kogler/Julia/tecnalia/Tests_Day_1/TC1.1_Box1.csv"
 file = CSV.File(path; delim=",", header=false)
 slack_meas = measurement(file...)
 
-path = "/home/kogler/Julia/tecnalia/Tests_Day_1/TC1.3_Box2.csv"
+path = "/home/kogler/Julia/tecnalia/Tests_Day_1/TC1.1_Box2.csv"
 file = CSV.File(path; delim=",", header=false)
 load_meas = measurement(file...)
 
@@ -60,12 +75,11 @@ load = ToComplex(load_meas)
 
 # plots
 ################################################################################
-tspan = (slack.t[1], slack.t[end])
-#tspan = (10., 12.)
+tspan = (slack.t[1]+1.0, slack.t[end]-1.0) # plot whole test
+#tspan = (11.22, 11.27) # plot specific time span (in seconds)
 idx = findall(x -> x>=tspan[1] && x<=tspan[2], slack.t)
-p1 = plot(slack.t[idx], [slack.uAmp[idx], load.uAmp[idx]], ylabel="|U|")
-p2 = plot(slack.t[idx], slack.iAmp[idx], ylabel="|I|")
-p3 = plot(slack.t[idx], [slack.f[idx], load.f[idx]], ylabel="f")
-p4 = plot(slack.t[idx], [slack.P[idx], load.P[idx]], ylabel="P")
-plot(p1, p2, p3, p4, layout=(4,1), label=["Slack" "Load"])
-
+p1 = plot(slack.t[idx], [slack.uAmp[idx], load.uAmp[idx]], ylabel="|u| [pu]")
+p2 = plot(slack.t[idx], [slack.f[idx], load.f[idx]], ylabel="f [Hz]")
+p3 = plot(slack.t[idx], [slack.P[idx], load.P[idx]], ylabel="P [pu]")
+plot(p1, p2, p3, layout=(3,1), label=["Slack" "Load"], legend=:outertopright)
+#savefig("Testcase1-1_results") # save as .png
