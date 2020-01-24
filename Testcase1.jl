a1a828c3e14ea1e990c5fb4a129ab44820389cb2
Raphael Kogler <kogler@pik-potsdam.de>
2020-01-24 15:46:21 +0100

    improved frequency

--- Testcase1.jl
+++ Testcase1.jl
@@ -1,10 +1,13 @@
 using LinearAlgebra
 using CSV
 using Plots
+using FFTW
 
 VBase = 400
 PBase = 1e4
 IBase = PBase/VBase
+resolution = 1e-3
+samplerate = trunc(Int, 1/resolution)
 
 # define structs for measurement data and complex phasor representation
 ################################################################################
@@ -33,10 +36,29 @@ function averageWin(data, winSize::Int)
     return output
 end
 
+function getFreqAvg(data, rate)
+    data_fft = rfft(data)
+    val, fInd = findmax(abs2.(data_fft[2:end]))
+    fSteps = rfftfreq(length(data), rate)
+    fAvgOut = fSteps[fInd+1]
+    return fAvgOut
+end
+
+function getFreqInst(data, res, rate)
+    len = length(data)
+    ddt1 = zeros(len)
+    ddt3 = zeros(len)
+    for k in 3:len-2
+        ddt1[k] = (data[k+1] - data[k-1])/(2.0*res)
+        ddt3[k] = (data[k+2]/2.0 - data[k+1] + data[k-1] - data[k-2]/2.0)/res^3
+    end
+    fInstOut = (ddt1 .- res^2/6.0*ddt3)/(2.0*pi)
+    return fInstOut
+end
+
 function ToComplex(meas::measurement)
-    f = meas.f
-    fAvg = sum(f[1000:end])/length(f[1000:end])
-    win = trunc(Int, 1/(meas.t[2]*fAvg))
+    fAvg = getFreqAvg(Array{Float64}(meas.U1), samplerate)
+    win = trunc(Int, 1/(resolution*fAvg))
     u_ab = ABTransform*transpose(hcat(meas.U1, meas.U2, meas.U3))
     u = (u_ab[1,:] .+ im*u_ab[2,:])/VBase
     uRot = u.*exp.(im*2*pi*fAvg*meas.t)
@@ -49,6 +71,7 @@ function ToComplex(meas::measurement)
     iRotAvg = averageWin(iRot, win)
     iAmp = abs.(iRotAvg)
     iPhi = angle.(iRotAvg)
+    f = fAvg .+ getFreqInst(uPhi, resolution, samplerate)
     S = uRotAvg.*conj.(iRotAvg)
     P = real.(S)
     Q = imag.(S)
@@ -76,7 +99,7 @@ load = ToComplex(load_meas)
 # plots
 ################################################################################
 tspan = (slack.t[1]+1.0, slack.t[end]-1.0) # plot whole test
-#tspan = (11.22, 11.27) # plot specific time span (in seconds)
+tspan = (10., 10.5) # plot specific time span (in seconds)
 idx = findall(x -> x>=tspan[1] && x<=tspan[2], slack.t)
 p1 = plot(slack.t[idx], [slack.uAmp[idx], load.uAmp[idx]], ylabel="|u| [pu]")
 p2 = plot(slack.t[idx], [slack.f[idx], load.f[idx]], ylabel="f [Hz]")
